5506256b42715e85c93948ea42672bf4ebb0ac78
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-01-30 22:16:21 +0600

    form functionality added

---
+++ components/forms/onboarding/CompanyForm.tsx
@@ -0,0 +1,162 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "@/app/utils/zodSchemas";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { countryList } from "@/app/utils/countriesList";
+import { Textarea } from "@/components/ui/textarea";
+import { UploadDropzone } from "@/components/general/UploadThing";
+
+const CompanyForm = () => {
+  const form = useForm<z.infer<typeof companySchema>>({
+    resolver: zodResolver(companySchema),
+    defaultValues: {
+      about: "",
+      location: "",
+      logo: "",
+      name: "",
+      website: "",
+      xAccount: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form className="space-y-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter company name" {...field} />
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Company Location</FormLabel>
+                <Select
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue placeholder="Select Location" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>Worldwide</SelectLabel>
+                      <SelectItem value="worldwide">
+                        <span>🌏</span> <span>Worldwide / Remote</span>
+                      </SelectItem>
+                    </SelectGroup>
+                    <SelectGroup>
+                      <SelectLabel>Location</SelectLabel>
+                      {countryList.map((country) => (
+                        <SelectItem value={country.name} key={country.code}>
+                          <span>{country.flagEmoji}</span>
+                          <span className="pl-2">{country.name}</span>
+                        </SelectItem>
+                      ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
+          <FormField
+            control={form.control}
+            name="website"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website</FormLabel>
+                <FormControl>
+                  <Input placeholder="https://www.yourcompany.com" {...field} />
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="xAccount"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>X (Twitter) Account</FormLabel>
+                <FormControl>
+                  <Input placeholder="@yourcompany" {...field} />
+                </FormControl>
+                <FormMessage></FormMessage>
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="about"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>About</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell use about your company..."
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage></FormMessage>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="logo"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Logo</FormLabel>
+              <FormControl>
+                <UploadDropzone
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    field.onChange(res[0].url);
+                  }}
+                  onUploadError={() => console.error("Something went wrong")}
+                />
+              </FormControl>
+              <FormMessage></FormMessage>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
+
+export default CompanyForm;
---
+++ components/forms/onboarding/OnboardingForm.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Logo from "@/public/logo.png";
+import { Card, CardContent } from "@/components/ui/card";
+import UserTypeSelection from "./UserTypeForm";
+import CompanyForm from "./CompanyForm";
+
+export type UserSelectionType = "company" | "jobSeeker" | null;
+
+const OnboardingForm = () => {
+  const [step, setStep] = useState(1);
+  const [userType, setUserType] = useState<UserSelectionType>(null);
+
+  function handleUserTypeSelection(type: UserSelectionType) {
+    setUserType(type);
+    setStep(2);
+  }
+
+  function renderStep() {
+    switch (step) {
+      case 1:
+        return <UserTypeSelection onSelect={handleUserTypeSelection} />;
+      case 2:
+        return userType === "company" ? (
+          <CompanyForm />
+        ) : (
+          <p>User is a job seeker</p>
+        );
+      default:
+        return null;
+    }
+  }
+
+  return (
+    <>
+      <div className="flex items-center gap-4 mb-10">
+        <Image src={Logo} alt="Job Marshal Logo" width={50} height={50} />
+        <h1 className="text-2xl font-bold">
+          Job<span className="text-primary">Marshal</span>
+        </h1>
+      </div>
+      <Card className="max-w-lg w-full">
+        <CardContent className="p-6">{renderStep()}</CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default OnboardingForm;
