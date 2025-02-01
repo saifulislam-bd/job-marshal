efa5681273edead6fc226b1cc7a32c5b7f689240
saifulislam-bd <saifulislam.dev22@gmail.com>
2025-02-01 19:50:29 +0600

    add Arcjet and secured the app

--- app/actions.ts
+++ app/actions.ts
@@ -5,9 +5,31 @@ import { requireUser } from "./utils/requireUser";
 import { companySchema, jobSeekerSchema } from "./utils/zodSchemas";
 import { prisma } from "./utils/db";
 import { redirect } from "next/navigation";
+import arcjet, { detectBot, shield } from "./utils/arcjet";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    shield({
+      mode: "LIVE",
+    })
+  )
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  );
 
 export async function createCompany(data: z.infer<typeof companySchema>) {
   const session = await requireUser();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("FORBIDDEN");
+  }
 
   const validateData = companySchema.parse(data);
   await prisma.user.update({
@@ -29,6 +51,13 @@ export async function createCompany(data: z.infer<typeof companySchema>) {
 
 export async function createJobSeeker(data: z.infer<typeof jobSeekerSchema>) {
   const user = await requireUser();
+
+  const req = await request();
+  const decision = await aj.protect(req);
+
+  if (decision.isDenied()) {
+    throw new Error("FORBIDDEN");
+  }
 
   const validateData = jobSeekerSchema.parse(data);
   await prisma.user.update({
